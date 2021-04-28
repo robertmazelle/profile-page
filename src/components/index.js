ddcecdde2b50775b825f4f2809ce6b4ed7c4eaf0
robertmazelle <robert-mazelle@gmail.com>
2021-04-28 18:29:50 +0200

    exported all components in one file

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,7 @@
-export { default as Topbar } from './Topbar';
 export { default as Button } from './Button';
+export { default as EducationItem } from './EducationItem/EducationItem';
+export { default as HobbiesPanel } from './HobbiesPanel/HobbiesPanel';
+export { default as ImageChanger } from './ImageChanger/ImageChanger';
 export { default as Seo } from './Seo';
-export { default as WorkItem } from './WorkItem';
+export { default as Topbar } from './Topbar/Topbar';
+export { default as WorkItem } from './WorkItem/WorkItem';
